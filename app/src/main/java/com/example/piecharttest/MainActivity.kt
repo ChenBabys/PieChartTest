196d3ec5c454c0f76a51f1be42fb7bb24f14fc5e
ChenYeSheng <1584935420@qq.com>
2021-10-25 20:30:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/piecharttest/MainActivity.kt
@@ -0,0 +1,146 @@
+package com.example.piecharttest
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.MPPointF
+
+class MainActivity : AppCompatActivity(), OnChartValueSelectedListener  {
+
+    private lateinit var chart: PieChart
+
+    private val parties = arrayOf(
+        "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
+        "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
+        "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
+        "Party Y", "Party Z"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        chart = findViewById(R.id.pie_chart)
+        initView()
+    }
+
+    private fun initView() {
+        chart.setUsePercentValues(true)
+        chart.description.isEnabled = false
+        chart.setExtraOffsets(5f,10f,5f,5f)
+        chart.dragDecelerationFrictionCoef = 0.95f
+        chart.centerText = "测试测试"
+
+        chart.isDrawHoleEnabled = true
+        chart.setHoleColor(Color.WHITE)
+
+        chart.setTransparentCircleColor(Color.WHITE)
+        chart.setTransparentCircleAlpha(110)
+
+        chart.holeRadius = 58f
+        chart.transparentCircleRadius = 61f
+
+        chart.setDrawCenterText(true)
+
+        chart.rotationAngle = 0f
+        // enable rotation of the chart by touch
+        // enable rotation of the chart by touch
+        chart.isRotationEnabled = true
+        chart.isHighlightPerTapEnabled = true
+        // add a selection listener
+
+        // chart.setUnit(" €");
+        // chart.setDrawUnitsInChart(true);
+
+        // add a selection listener
+        chart.setOnChartValueSelectedListener(this)
+        chart.animateY(1400, Easing.EaseInOutQuad)
+        // chart.spin(2000, 0, 360);
+
+        // chart.spin(2000, 0, 360);
+        val l = chart.legend
+        l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        l.orientation = Legend.LegendOrientation.VERTICAL
+        l.setDrawInside(false)
+        l.xEntrySpace = 7f
+        l.yEntrySpace = 0f
+        l.yOffset = 0f
+
+        // entry label styling
+
+        // entry label styling
+        chart.setEntryLabelColor(Color.WHITE)
+        chart.setEntryLabelTextSize(12f)
+        setData(5, 5f)
+    }
+
+    private fun setData(count: Int, range: Float) {
+        val entries = ArrayList<PieEntry>()
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (i in 0 until count) {
+            entries.add(
+                PieEntry(
+                    (Math.random() * range + range / 5).toFloat(),
+                    parties.get(i % parties.size),
+                    //resources.getDrawable(R.drawable.star)
+                   ContextCompat.getDrawable(this,R.drawable.star)
+                )
+            )
+        }
+        val dataSet = PieDataSet(entries, "Election Results")
+        dataSet.setDrawIcons(false)
+        dataSet.sliceSpace = 3f
+        dataSet.iconsOffset = MPPointF(0f, 40f)
+        dataSet.selectionShift = 5f
+
+        // add a lot of colors
+        val colors = ArrayList<Int>()
+        for (c in ColorTemplate.VORDIPLOM_COLORS) colors.add(c)
+        for (c in ColorTemplate.JOYFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.COLORFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.LIBERTY_COLORS) colors.add(c)
+        for (c in ColorTemplate.PASTEL_COLORS) colors.add(c)
+        colors.add(ColorTemplate.getHoloBlue())
+        dataSet.colors = colors
+        //dataSet.setSelectionShift(0f);
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter())
+        data.setValueTextSize(11f)
+        data.setValueTextColor(Color.WHITE)
+        chart.data = data
+
+        // undo all highlights
+        chart.highlightValues(null)
+        chart.invalidate()
+    }
+
+    override fun onValueSelected(e: Entry?, h: Highlight?) {
+        if (e == null) return
+        Log.i(
+            "VAL SELECTED",
+            "Value: " + e.y + ", index: " + h!!.x
+                    + ", DataSet index: " + h!!.dataSetIndex
+        )
+    }
+
+    override fun onNothingSelected() {
+        Log.i("PieChart", "nothing selected")
+    }
+
+
+}
